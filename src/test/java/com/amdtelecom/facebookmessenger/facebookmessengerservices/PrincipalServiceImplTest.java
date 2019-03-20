78b06af8127f8210df95a5cc8c48b5fc6eeedc56
Vivek Selvarajan <agent47vivek@gmail.com>
2019-03-20 23:08:52 +0530

    --Unit Test for principal 2/3

---
+++ src/test/java/com/amdtelecom/facebookmessenger/facebookmessengerservices/PrincipalServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.amdtelecom.facebookmessenger.facebookmessengerservices;
+
+import com.amdtelecom.facebookmessenger.facebookmessengerservices.exceptions.InvalidPrincipalException;
+import com.amdtelecom.facebookmessenger.facebookmessengerservices.models.MessengerServicePrincipal;
+import com.amdtelecom.facebookmessenger.facebookmessengerservices.repositories.PrincipalRepository;
+import com.amdtelecom.facebookmessenger.facebookmessengerservices.services.PrincipalServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrincipalServiceImplTest {
+    @Mock
+    private PrincipalRepository principalRepository;
+    @InjectMocks
+    private PrincipalServiceImpl principalServiceImpl;
+
+    @Test
+    public void testCreatePrincipal() {
+        String mockPrincipalId = "mock-principal-id-123-5678";
+        Map<String, String> metadata = new HashMap();
+        metadata.put("key1", "value1");
+        metadata.put("key2", "value2");
+        MessengerServicePrincipal mockPrincipal = new MessengerServicePrincipal(mockPrincipalId, "platform-id-1", metadata, new Date(), new Date());
+        when(principalRepository.save(mockPrincipal)).thenReturn(mockPrincipal);
+        assertEquals(null, principalServiceImpl.createPrincipal("platform-id-1", metadata));
+    }
+
+    @Test
+    public void testGetPrincipalDetails() throws InvalidPrincipalException {
+        String mockPlatformId = "mock-platform-id-123-5678";
+        String mockPrincipalId = "mock-principal-id-123-5678";
+        Map<String, String> metadata = new HashMap();
+        metadata.put("key1", "value1");
+        metadata.put("key2", "value2");
+        MessengerServicePrincipal mockPrincipal = new MessengerServicePrincipal(mockPrincipalId, mockPlatformId, metadata, new Date(), new Date());
+        when(principalRepository.getMessengerServicePrincipalByPrincipalId(mockPlatformId, mockPrincipalId)).thenReturn(mockPrincipal);
+        System.out.println(mockPrincipal);
+        assertEquals("mock-principal-id-123-5678", principalServiceImpl.getSpecificPrincipal(mockPlatformId, mockPrincipalId).getPrincipalId());
+    }
+}
