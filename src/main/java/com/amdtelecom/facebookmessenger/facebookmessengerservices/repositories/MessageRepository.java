b6bfe463d3b93bbc4a8781eeb4fc505e1f5613c2
Vivek Selvarajan <vivek.selvarajan@people10.com>
2019-03-26 11:07:50 +0530

    -- message repository

--- src/main/java/com/amdtelecom/facebookmessenger/facebookmessengerservices/repositories/MessageRepository.java
+++ src/main/java/com/amdtelecom/facebookmessenger/facebookmessengerservices/repositories/MessageRepository.java
@@ -1,8 +1,9 @@
 package com.amdtelecom.facebookmessenger.facebookmessengerservices.repositories;
 
+import com.amdtelecom.facebookmessenger.facebookmessengerservices.models.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.query.MongoParameterAccessor;
 
-public interface MessageRepository extends MongoRepository<T, ID>, MongoParameterAccessor {
+public interface MessageRepository extends MongoRepository<Message, String>{
 
 }
